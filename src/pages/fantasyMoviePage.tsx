88585bea8bfa9df552c503793da0c6ecf3d60043
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-07-13 17:51:45 +0100

    Changing fantasyMoviePage to display form on left and list on right

--- src/pages/fantasyMoviePage.tsx
+++ src/pages/fantasyMoviePage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import FantasyMovieList from "../components/fantasyMovieList";
 import FantasyMovieForm from "../components/fantasyMovieForm";
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 
 const FantasyMoviePage: React.FC = () => {
   return (
     <Container>
-      <FantasyMovieList />
-      <FantasyMovieForm />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <FantasyMovieForm />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FantasyMovieList />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
