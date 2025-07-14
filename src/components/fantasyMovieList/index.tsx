d9fdae2bc8ab8413efb04429da0066a037972ea5
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-07-14 19:13:23 +0100

    Removing grid from fantasyMovieList as 1 is being displayed per row

--- src/components/fantasyMovieList/index.tsx
+++ src/components/fantasyMovieList/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Box, Card, CardContent, Chip, Grid, Typography } from "@mui/material";
+import { Box, Card, CardContent, Chip, Typography } from "@mui/material";
 import { MoviesContext } from "../../contexts/moviesContext";
 
 const FantasyMovieList: React.FC = () => {
@@ -7,35 +7,33 @@ const FantasyMovieList: React.FC = () => {
 
   // Code currently taken and slightly modified from https://github.com/eoinfennessy/movies-app/ to get started. Will be significantly changed later
   return (
-    <>
-      <Box mt={2}>
-        <Typography variant="h3">My Fantasy Movies</Typography>
-        <Grid container spacing={2}>
-          {fantasyMovies.map((movie, index) => (
-            <Grid item xs={4}>
-              <Card key={index}>
-                <CardContent>
-                  <Typography variant="h5">{movie.title}</Typography>
-                  {movie.genres.map((genre) => (
-                    <Chip label={genre} color="primary" />
-                  ))}
-                  <Typography variant="body2">{movie.overview}</Typography>
-                  <Typography variant="body1">
-                    Release Date: {movie.releaseDate?.toLocaleDateString()}
-                  </Typography>
-                  <Typography variant="body1">
-                    Runtime: {movie.runtime} minutes
-                  </Typography>
-                  {movie.productionCompanies.map((company) => (
-                    <Chip label={company} color="secondary" />
-                  ))}
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+    <Box mt={2}>
+      <Typography variant="h3">My Fantasy Movies</Typography>
+      <Box>
+        {[...fantasyMovies].reverse().map((movie, index) => (
+          <Box key={index} mb={2}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5">{movie.title}</Typography>
+                {movie.genres.map((genre) => (
+                  <Chip key={genre} label={genre} color="primary" />
+                ))}
+                <Typography variant="body2">{movie.overview}</Typography>
+                <Typography variant="body1">
+                  Release Date: {movie.releaseDate?.toLocaleDateString()}
+                </Typography>
+                <Typography variant="body1">
+                  Runtime: {movie.runtime} minutes
+                </Typography>
+                {movie.productionCompanies.map((company) => (
+                  <Chip key={company} label={company} color="secondary" />
+                ))}
+              </CardContent>
+            </Card>
+          </Box>
+        ))}
       </Box>
-    </>
+    </Box>
   );
 };
 
