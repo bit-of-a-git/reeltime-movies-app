e3e8fdd715613141b11374dbc27a4c4a47a069a3
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-08-01 22:19:57 +0100

    Refactor MovieHeader component: remove unused icons, fixing issue when favourites are empty, clean up JSX structure.

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import { MovieDetailsProps } from "../../types/interfaces";
@@ -23,14 +20,13 @@ const styles = {
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
-  const favourites = JSON.parse(localStorage.getItem("favourites"));
-  const isFavourite = favourites.some((fav) => fav.id === movie.id);
+  const favourites = JSON.parse(localStorage.getItem("favourites") || "[]");
+  const isFavourite = favourites.some(
+    (fav: { id: number }) => fav.id === movie.id
+  );
 
   return (
     <Paper component="div" sx={styles.root}>
-      <IconButton aria-label="go back">
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
       {isFavourite && (
         <Avatar sx={styles.avatar}>
           <FavoriteIcon />
@@ -38,16 +34,13 @@ const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
       )}
       <Typography variant="h4" component="h3">
         {movie.title}
-        {"   "}
-        <a href={movie.homepage}>
-          <HomeIcon color="primary" fontSize="large" />
-        </a>
-        <br />
-        <span>{`${movie.tagline}`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
+      <Typography variant="h5" component="h3">
+        {movie.tagline}
+      </Typography>
+      <a href={movie.homepage} target="_blank" rel="noopener noreferrer">
+        <HomeIcon color="primary" fontSize="large" />
+      </a>
     </Paper>
   );
 };
