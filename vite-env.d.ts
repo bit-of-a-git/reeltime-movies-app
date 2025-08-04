eb1b4f6dd783927671c131431197d9a06381ef7e
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-08-04 15:29:23 +0100

    Add vite-env.d.ts file with type references for Vite client and importMeta

---
+++ vite-env.d.ts
@@ -0,0 +1,4 @@
+// https://stackoverflow.com/questions/78114219/property-env-does-not-exist-on-type-importmeta-ts2339
+
+/// <reference types="vite/client" />
+/// <reference types="vite/types/importMeta.d.ts" />
