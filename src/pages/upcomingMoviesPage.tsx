ff7571f3c509eebeff14ff2dc337c054125b31f2
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-08-06 21:25:19 +0100

    Address CodeRabbit DRY comment by making generic MovieListPage and passing in title, queryKey, and fetch function. Refactor 5 movie pages to use this list

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -1,46 +1,12 @@
-import React, { useState } from "react";
-import PageTemplate from "../components/templateMovieListPage";
+import MovieListPage from "./movieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
-import { BaseMovieProps, UpcomingMovies } from "../types/interfaces";
-import { useQuery } from "react-query";
-import Spinner from "../components/spinner";
-import AddToMustWatchIcon from "../components/cardIcons/addToMustWatch";
 
-const UpcomingMoviesPage: React.FC = () => {
-  const [page, setPage] = useState(1);
+const UpcomingMoviesPage: React.FC = () => (
+  <MovieListPage
+    title="Upcoming Movies"
+    queryKey="upcomingMovies"
+    fetchFunction={getUpcomingMovies}
+  />
+);
 
-  const { data, error, isLoading, isError } = useQuery<UpcomingMovies, Error>(
-    ["upcoming", page],
-    () => getUpcomingMovies(page),
-    { keepPreviousData: true }
-  );
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
-
-  const movies = data ? data.results : [];
-
-  const changePage = (delta: number) => {
-    const newPage = page + delta;
-    if (newPage > 0 && newPage <= data.total_pages) {
-      setPage(newPage);
-    }
-  };
-
-  return (
-    <PageTemplate
-      title="Upcoming Movies"
-      movies={movies}
-      action={(movie: BaseMovieProps) => {
-        return <AddToMustWatchIcon {...movie} />;
-      }}
-      changePage={changePage}
-    />
-  );
-};
 export default UpcomingMoviesPage;
